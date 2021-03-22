e85ba544d7b2a4f21a4698eaa300ef644437d9ac
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2021-03-22 11:46:37 +0100

    Add implementation for Complex trait

--- src/u04lab/code/Complex.scala
+++ src/u04lab/code/Complex.scala
@@ -8,20 +8,21 @@ trait Complex {
 }
 
 object Complex {
-  def apply(re:Double, im:Double):Complex = ??? // Fill here
+  def apply(re: Double, im: Double): Complex = ComplexImpl(re, im)
+
+  private case class ComplexImpl(override val re: Double, override val im: Double) extends Complex {
+    override def +(c: Complex): Complex = ComplexImpl(re + c.re, im + c.im)
+
+    override def *(c: Complex): Complex = ComplexImpl(re * c.re - im * c.im, re * c.im + im * c.re)
+  }
 }
 
 object TryComplex extends App {
-  val a = Array(Complex(10,20), Complex(1,1), Complex(7,0))
+  val a = Array(Complex(10, 20), Complex(1, 1), Complex(7, 0))
   val c = a(0) + a(1) + a(2)
+  val b = Complex(10, 20)
   println(c, c.re, c.im) // (ComplexImpl(18.0,21.0),18.0,21.0)
   val c2 = a(0) * a(1)
   println(c2, c2.re, c2.im) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+  println(a(0) == b)
 }
-
-/** Hints:
-  * - implement Complex with a ComplexImpl class, similar to PersonImpl in slides
-  * - check that equality and toString do not work
-  * - use a case class ComplexImpl instead, creating objects without the 'new' keyword
-  * - check equality and toString now
-  */
